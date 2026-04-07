04e824a572cba9285ab25b40d11fb2cdf606b8f5
langchain-math-agent-gemini <168328249+TechyLakshay@users.noreply.github.com>
2026-04-08 00:25:58 +0530

    remove .env from .gitignore and delete test.py file

--- test.py
+++
@@ -1,5 +0,0 @@
-from groq import Groq
-print(Groq(api_key="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").chat.completions.create(
-    model="openai/gpt-oss-20b",
-    messages=[{"role":"user","content":"write a code  for printing fabonacci series in python"}]
-).choices[0].message.content) 
